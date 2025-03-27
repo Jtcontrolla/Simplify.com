b49721b8a83e633f3db74ed9d90fa440bd6024b8
Timothy-Jacob Miyenseigha Marvellous <timothyjacobmarvelous@gmail.com>
2025-03-27 11:31:06 +0100

    project routing

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,7 +6,7 @@ import Work3 from '../assets/Work3.png'
 
 export default function Projects () {
     return (
-        <section className="bg-[#19191B]" id="services">
+        <section className="bg-[#19191B]" id="projects">
             <h2 className="text-white font-[Roboto] text-5xl font-[600] leading-[70.6px] pt-[44px] w-[593px] ml-15 text-center mt-35">Our Awesome Portfolio</h2>
 
             <div className="absolute top-[2400px] right-[130px]">
